2f6ab7ecc1e59dc6df47e2e7f0f29423931625bc
JiananHe <15051857858@qq.com>
2019-10-06 16:49:43 +0800

    data_loader and model finished

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -5,7 +5,10 @@
 
 import torch
 import SimpleITK as sitk
-from torch.utils.data import dataset
+import scipy.ndimage as ndimage
+from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
+import torch.nn.functional as F
 import os
 import csv
 import random
@@ -15,9 +18,41 @@ image_path = os.path.join(sample_path, 'image')
 label_path = os.path.join(sample_path, 'label')
 
 sample_slices = 48  # 48*256*256 for a sample
+lower = -350
 
 
-class Dataset(dataset):
+def produceRandomlyDeformedImage(sitkImage, sitklabel, numcontrolpoints, stdDef):
+    transfromDomainMeshSize=[numcontrolpoints]*sitkImage.GetDimension()
+
+    tx = sitk.BSplineTransformInitializer(sitkImage, transfromDomainMeshSize)
+
+    params = tx.GetParameters()
+
+    paramsNp=np.asarray(params,dtype=float)
+    paramsNp = paramsNp + np.random.randn(paramsNp.shape[0])*stdDef
+
+    paramsNp[0:int(len(params)/3)]=0 #remove z deformations! The resolution in z is too bad
+
+    params=tuple(paramsNp)
+    tx.SetParameters(params)
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(sitkImage)
+    resampler.SetDefaultPixelValue(0)
+    resampler.SetTransform(tx)
+
+    resampler.SetInterpolator(sitk.sitkLinear)
+    outimgsitk = resampler.Execute(sitkImage)
+
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+    outlabsitk = resampler.Execute(sitklabel)
+
+    # save
+    sitk.WriteImage(outimgsitk, r'D:/image-test.nii')
+    sitk.WriteImage(outlabsitk, r'D:/label-test.nii')
+
+
+class MyDataset(Dataset):
     def __init__(self, csv_path):
         if 'train' in csv_path:
             self.is_training = True
@@ -31,11 +66,11 @@ class Dataset(dataset):
         return len(self.sample_ids)
 
     def __getitem__(self, item):
-        id = self.sample_ids[item]
+        id = int(self.sample_ids[item])
 
         # 读取
-        image = sitk.ReadImage(os.path.join(image_path, "%image04d.nii" % id))
-        label = sitk.ReadImage(os.path.join(label_path, "%label04d.nii" % id))
+        image = sitk.ReadImage(os.path.join(image_path, "image%04d.nii" % id))
+        label = sitk.ReadImage(os.path.join(label_path, "label%04d.nii" % id))
         img_array = sitk.GetArrayFromImage(image)
         lbl_array = sitk.GetArrayFromImage(label)
 
@@ -47,4 +82,57 @@ class Dataset(dataset):
 
         # 数据增强
         if self.is_training:
+            # 以0.5的概率在5度的范围内随机旋转
+            # 角度为负数是顺时针旋转，角度为正数是逆时针旋转
+            if random.uniform(0, 1) >= 0.5:
+                angle = random.uniform(-5, 5)
+                img_array = ndimage.rotate(img_array, angle, axes=(1, 2), reshape=False, cval=lower)
+                lbl_array = ndimage.rotate(lbl_array, angle, axes=(1, 2), reshape=False, cval=0)
+
+            # 有0.5的概率不进行任何修修改，剩下0.5随机挑选0.8-0.5大小的patch放大到256*256
+            if random.uniform(0, 1) >= 0.5:
+                img_array, lbl_array = self.zoom(img_array, lbl_array, patch_size=random.uniform(0.5, 0.8))
+
+
+        # 处理完毕，将array转换为tensor
+        img_array = torch.FloatTensor(img_array).unsqueeze(0)
+        lbl_array = torch.FloatTensor(lbl_array)
+
+        return img_array, lbl_array
+
+
+    def zoom(self, ct_array, seg_array, patch_size):
+        length = int(256 * patch_size)
+
+        x1 = int(random.uniform(0, 255 - length))
+        y1 = int(random.uniform(0, 255 - length))
+
+        x2 = x1 + length
+        y2 = y1 + length
+
+        ct_array = ct_array[:, x1:x2 + 1, y1:y2 + 1]
+        seg_array = seg_array[:, x1:x2 + 1, y1:y2 + 1]
+
+        with torch.no_grad():
+            ct_array = torch.FloatTensor(ct_array).unsqueeze(dim=0).unsqueeze(dim=0)
+            ct_array = Variable(ct_array)
+            ct_array = F.upsample(ct_array, (sample_slices, 256, 256), mode='trilinear').squeeze().detach().numpy()
+
+            seg_array = torch.FloatTensor(seg_array).unsqueeze(dim=0).unsqueeze(dim=0)
+            seg_array = Variable(seg_array)
+            seg_array = F.upsample(seg_array, (sample_slices, 256, 256), mode='nearest').squeeze().detach().numpy()
+
+            return ct_array, seg_array
+
+
+if __name__ == "__main__":
+    train_ds = MyDataset('D:/Projects/OrgansSegment/Data/data_preprocess/train_info.csv')
+
+    # 测试代码
+    train_dl = DataLoader(train_ds, 6, True)
+    for index, (ct, seg) in enumerate(train_dl):
+
+        print(index, ct.size(), seg.size())
+        print('----------------')
+
 
---
+++ model/vnet.py
@@ -0,0 +1,233 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+
+dropout_rate = 0.3
+num_organ = 8
+
+class VNet(nn.Module):
+    def __init__(self, training, inchannel, stage):
+        """
+        :param training: training or test
+        :param inchannel: 网络最开始的输入通道数量
+        :param stage: 第一个VNet还是第二个VNet
+        """
+        super().__init__()
+        self.training = training
+        self.stage = stage
+
+        self.encoder_stage1 = nn.Sequential(
+            nn.Conv3d(inchannel, 16, 3, 1, padding=1),
+            nn.PReLU(16)
+        )
+        self.down_conv1 = nn.Sequential(
+            nn.Conv3d(16, 32, 2, 2),
+            nn.PReLU(32)
+        )
+
+        self.encoder_stage2 = nn.Sequential(
+            nn.Conv3d(32, 32, 3, 1, padding=1),
+            nn.PReLU(32),
+            nn.Conv3d(32, 32, 3, 1, padding=1),
+            nn.PReLU(32),
+        )
+        self.down_conv2 = nn.Sequential(
+            nn.Conv3d(32, 64, 2, 2),
+            nn.PReLU(64)
+        )
+
+        self.encoder_stage3 = nn.Sequential(
+            nn.Conv3d(64, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+            nn.Conv3d(64, 64, 3, 1, padding=2, dilation=2),
+            nn.PReLU(64),
+            nn.Conv3d(64, 64, 3, 1, padding=4, dilation=4),
+            nn.PReLU(64),
+        )
+        self.down_conv3 = nn.Sequential(
+            nn.Conv3d(64, 128, 2, 2),
+            nn.PReLU(128)
+        )
+
+        self.encoder_stage4 = nn.Sequential(
+            nn.Conv3d(128, 128, 3, 1, padding=3, dilation=3),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=4, dilation=4),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=5, dilation=5),
+            nn.PReLU(128),
+        )
+        self.down_conv4 = nn.Sequential(
+            nn.Conv3d(128, 256, 3, 1, padding=1),
+            nn.PReLU(256)
+        )
+
+        self.decoder_stage1 = nn.Sequential(
+            nn.Conv3d(128, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+        )
+        self.up_conv1 = nn.Sequential(
+            nn.ConvTranspose3d(256, 128, 2, 2),
+            nn.PReLU(128)
+        )
+
+        self.decoder_stage2 = nn.Sequential(
+            nn.Conv3d(128 + 64, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+        )
+        self.up_conv2 = nn.Sequential(
+            nn.ConvTranspose3d(128, 64, 2, 2),
+            nn.PReLU(64)
+        )
+
+        self.decoder_stage3 = nn.Sequential(
+            nn.Conv3d(64 + 32, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+
+            nn.Conv3d(64, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+        )
+        self.up_conv3 = nn.Sequential(
+            nn.ConvTranspose3d(64, 32, 2, 2),
+            nn.PReLU(32)
+        )
+
+        self.decoder_stage4 = nn.Sequential(
+            nn.Conv3d(32 + 16, 32, 3, 1, padding=1),
+            nn.PReLU(32),
+        )
+        self.map = nn.Sequential(
+            nn.Conv3d(32, num_organ + 1, 1),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, inputs):
+        if self.stage is 'stage1':
+            encode1 = self.encoder_stage1(inputs) + inputs
+        else:
+            encode1 = self.encoder_stage1(inputs)
+
+        down1 = self.down_conv1(encode1)
+
+        encode2 = self.encoder_stage2(down1) + down1
+        encode2 = F.dropout(encode2, dropout_rate, self.training)
+
+        down2 = self.down_conv2(encode2)
+
+        encode3 = self.encoder_stage3(down2)
+        encode3 = encode3 + down2
+        encode3 = F.dropout(encode3, dropout_rate, self.training)
+
+        down3 = self.down_conv3(encode3)
+
+        encode4 = self.encoder_stage4(down3) + down3
+        encode4 = F.dropout(encode4, dropout_rate, self.training)
+
+        down4 = self.down_conv4(encode4)
+
+        decode1 = self.decoder_stage1(encode4) + down4
+        decode1 = F.dropout(decode1, dropout_rate, self.training)
+
+        up1 = self.up_conv1(decode1)
+
+        decode2 = self.decoder_stage2(torch.cat([up1, encode3], dim=1)) + up1
+        decode2 = F.dropout(decode2, dropout_rate, self.training)
+
+        up2 = self.up_conv2(decode2)
+
+        decode3 = self.decoder_stage3(torch.cat([up2, encode2], dim=1)) + up2
+        decode3 = F.dropout(decode3, dropout_rate, self.training)
+
+        up3 = self.up_conv3(decode3)
+
+        decode4 = self.decoder_stage4(torch.cat([up3, encode1], dim=1)) + up3
+
+        outputs = self.map(decode4)
+
+        return outputs
+
+
+# two VNet
+class Net(nn.Module):
+    def __init__(self, training):
+        super().__init__()
+
+        self.training = training
+
+        self.stage1 = VNet(training, inchannel=1, stage='stage1')
+        self.stage2 = VNet(training, inchannel=(num_organ+1)+1, stage='stage2')
+
+    def forward(self, inputs):
+        # 将第一阶段的输入缩小一倍
+        inputs_stage1 = F.upsample(inputs, (48, 128, 128), mode='trilinear')
+
+        outputs_stage1 = self.stage1(inputs_stage1)
+        outputs_stage1 = F.upsample(outputs_stage1, (48, 256, 256), mode='trilinear')
+
+        # 将第一阶段的输出与原始输入拼接插入第二阶段
+        inputs_stage2 = torch.cat([outputs_stage1, inputs], dim=1)
+
+        outputs_stage2 = self.stage2(inputs_stage2)
+
+        if self.training:
+            return outputs_stage1, outputs_stage2
+        else:
+            return outputs_stage2
+
+
+
+def get_net(training):
+    # 网络参数初始化函数
+    def init(module):
+        if isinstance(module, nn.Conv3d) or isinstance(module, nn.ConvTranspose3d):
+            nn.init.kaiming_normal(module.weight.data, 0.25)
+            nn.init.constant(module.bias.data, 0)
+
+    net = Net(training=training)
+    net.apply(init)
+
+    return net
+
+
+if __name__ == "__main__":
+    net = get_net(True)
+    net = net.cuda()
+    # summary(net, (1, 48, 256, 256))
+
+    data = torch.randn((1, 1, 48, 256, 256)).cuda()
+
+    with torch.no_grad():
+        out = net(data)
+
+    for item in out:
+        print(item.size())
+
+    # 计算参数个数
+    count = .0
+    for item in net.modules():
+        if isinstance(item, nn.Conv3d) or isinstance(item, nn.ConvTranspose3d):
+            count += (item.weight.size(0) * item.weight.size(1) *
+                      item.weight.size(2) * item.weight.size(3) * item.weight.size(4))
+
+            if item.bias is not None:
+                count += item.bias.size(0)
+
+        elif isinstance(item, nn.PReLU):
+            count += item.num_parameters
+
+    print("number of parameters: ", count)
